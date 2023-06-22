c192727d9350208aca2377cc97b15753830682a3
Higor de Deus <higordeus22@gmail.com>
2023-06-22 16:11:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace ScreenStreamingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebSocketServer(IPAddress.Any, 1234);
+            server.AddWebSocketService<ScreenStreaming>("/screen");
+            server.Start();
+            server.Stop();
+        }
+    }
+
+    class ScreenStreaming : WebSocketBehavior
+    {
+        private bool _isStreaming = false;
+
+        protected override void OnOpen()
+        {
+            _isStreaming = true;
+            Task.Run(() => StreamScreen());
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            _isStreaming = false;
+        }
+
+        private void StreamScreen()
+        {
+            var encoder = Encoder.Quality;
+            var parameters = new EncoderParameters(1);
+            parameters.Param[0] = new EncoderParameter(encoder, 50L);
+            var jpegEncoder = ImageCodecInfo.GetImageDecoders().FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
+            var socket = Context.WebSocket;
+
+            while (_isStreaming)
+            {
+                using (var bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
+                using (var graphics = Graphics.FromImage(bitmap))
+                using (var memoryStream = new MemoryStream())
+                {
+                    graphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+                    bitmap.Save(memoryStream, jpegEncoder, parameters);
+                    var buffer = memoryStream.ToArray();
+                    if (socket.ReadyState == WebSocketState.Open) 
+                    {
+                        socket.Send(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center">
+  <img src="screenshot.jpeg" alt="Screenshot" width="500">
+</p>
+
+# Screen Streaming Server :computer: :tv:
+
+> Um servidor de streaming de tela usando WebSocket em C#.
+
+Este projeto implementa um servidor de streaming de tela que captura a tela do computador e envia as imagens em tempo real para um cliente através de uma conexão WebSocket.
+
+## Tecnologias utilizadas
+
+- C#
+- .NET Framework
+- System.Drawing
+- WebSocketSharp
+
+## Funcionalidades
+
+- Streaming de tela em tempo real para um cliente WebSocket.
+- Compressão de imagens usando codificação JPEG.
+- Controle de início e parada do streaming através de abertura e fechamento da conexão WebSocket.
+
+## Como usar
+
+1. Clone este repositório: `git clone https://github.com/Higor-Matos/screen-streaming-server.git`
+2. Abra o projeto em sua IDE preferida.
+3. Compile e execute o projeto.
+4. Abra um cliente WebSocket e conecte-se ao servidor usando o caminho `/screen`.
+5. Desfrute do streaming de tela em tempo real!
+
+## Página de Streaming Pronta
+
+Se você deseja visualizar o streaming de tela em uma página web, confira o repositório [videoplayer](https://github.com/Higor-Matos/videoplayer). Ele fornece uma página de streaming pronta para receber o WebSocket do servidor e exibir o vídeo em tempo real.
+
+## Reportagem sobre o Projeto
+
+Caso queira assistir à reportagem sobre este projeto, confira o vídeo disponível em [YouTube](https://www.youtube.com/watch?v=dFQMuRxyf1c).
+
+## Contribuição
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir um "issue" ou enviar um "pull request" com melhorias, correções de bugs ou novos recursos.
+
